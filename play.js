2bcd51465f53b65894f2149226cd842bdee7fed4
Samy0412 <samantha.gadet@gmail.com>
2020-06-03 19:10:29 +0000

    CTRL + C again

--- play.js
+++ play.js
@@ -1,15 +1,15 @@
 const { connect } = require("./client");
-
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", (data) => {
-    if (data === "\u0003") {
-      process.exit();
-    }
-  });
+  stdin.on("data", handleUserInput);
 
   return stdin;
 };
