c9dfb614a7c094c98b8f86d2b4db4ff65dcf5a9a
Samy0412 <samantha.gadet@gmail.com>
2020-06-03 19:56:50 +0000

    constants module

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const { connect } = require("./client");
-const handleUserInput = function (data) {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
+const { setupInput } = require("./input");
 
-  return stdin;
-};
 console.log("Connecting ...");
-connect();
-setupInput();
+
+setupInput(connect());
